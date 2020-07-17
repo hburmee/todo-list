9d2fe844ff552e67a8747519f69b659cc82f382d
hburmee <37255507+hburmee@users.noreply.github.com>
2020-07-16 20:27:49 -0700

    complete

---
+++ assets/JS/app.js
@@ -0,0 +1,37 @@
+//check off specific todos
+
+
+$("ul").on("click","li", function(){
+
+	$(this).toggleClass("completed");
+// 	if($(this).css("color")==="rgb(128, 128, 128)"){
+// 		$(this).css({
+// 		color:"black",
+// 		textDecoration: "none"
+// 	});
+
+// 	}else{$(this).css({
+// 		color:"gray",
+// 		textDecoration: "line-through"
+// 	});
+// }
+	})
+
+$("ul").on("click", "span",function(event){
+	$(this).parent().fadeOut(function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span></span> " + todoText + "</li>");
+	}
+})
+
+$(".fa-plus").click(function(){
+	$("input[type = 'text']").fadeToggle();
+})
